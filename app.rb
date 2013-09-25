a4af77b39209da882b812b25a6e766cb6150df87
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2013-09-25 17:49:27 +0530

    added link and images

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
+require 'erubis'
+
 class MyApp
   def call env
-    [200, {"Content-Type" => "text/html"}, ["Welcome to App42 PaaS"]]
+    [200, {"Content-Type" => "text/html"}, [welcome]]
   end
+
+  def welcome
+    render :file => 'welcome'
+  end
+ 
+  def render(option)
+    @body = render_erb_file('views/' + option[:file] + '.erb')
+  end
+
+  def render_erb_file(file)
+    input = File.read(file)
+    eruby = Erubis::Eruby.new(input)
+    @body = eruby.result(binding())
+  end
+
 end
